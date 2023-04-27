b12c5e59d8b5166203d890f4e9de2559a6102d59
Luigi Otoya <Luigi.Otoya@gmail.com>
2023-04-27 16:07:52 -0400

    first commit

---
+++ public/client.js
@@ -0,0 +1,6 @@
+(function() {
+    const messages = document.getElementById("messages");
+    const textField = document.getElementById("textField");
+    const postBtn = document.getElementById("post-btn");
+    
+})();
